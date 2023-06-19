d9f8d4a546a9356fe7ec5d76f8e39616cafd1720
Clive Sasaka <clivesasaka@gmail.com>
2023-06-19 19:49:49 +0300

    Add database to the package

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,15 @@
 '''THis creates a package which enables the import of modules and classes'''
 
 from models.booking import booking
+from models.database.database_storage import Database_Storage
 from models.dwelling_type import dwelling_type
 from models.location import location
 from models.review import review
 from models.room_listing import room_listing
 from models.roommate_listing import roommate_listing
 from models.user_profile import user_profile
+
+
+database = Database_Storage()
+database.reload()
 
