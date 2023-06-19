63abba496a7778f358905e43eb3c3d8b9496fa3e
Clive Sasaka <clivesasaka@gmail.com>
2023-06-19 19:43:30 +0300

    Make a package and add roommate listing

---
+++ models/roommate_listing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This is the roommate listing module"""
+
+
+def roommate_listing():
+    """This is the roommate listing class"""
+    user_id = ""
+    roommate_id = ""
+    roommate_description = ""
+    roommate_price = ""
+    roommate_availability = ""
+    roommate_address = ""
+    roommate_status = ""
+    roommate_preference = ""
