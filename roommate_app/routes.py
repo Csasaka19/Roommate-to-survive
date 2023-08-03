64382e1639b54f3fdadd5f6e74349ea0c6b1ca7f
Clive Sasaka <clivesasaka@gmail.com>
2023-08-03 18:35:01 +0300

    Add the homepage to the application

--- roommate_app/routes.py
+++ roommate_app/routes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from roommate_app import roommate_app
+from flask import render_template
 
 @roommate_app.route('/')
 @roommate_app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
