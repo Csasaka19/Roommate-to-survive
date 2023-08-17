47bff0d0c9ec3d1124e3d58103e6dde6d31376c5
Clive Sasaka <clivesasaka@gmail.com>
2023-08-17 22:07:20 +0300

    Add a shell in context of the roommate_app

--- roommate_app.py
+++ roommate_app.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 '''This module defines the application object for the application'''
-from roommate_app import roommate_app
+from roommate_app import roommate_app, roommate_db
+from roommate_app.models import User, Review
+
+
+# This decorator registers a function to be executed right before the application receives a request
+@roommate_app.shell_context_processor
+# This function returns a dictionary of items to be imported when the shell starts up
+# Run the following command in the terminal to start the shell: $ flask shell
+def make_shell_context():
+    '''This function creates a shell context for the application'''
+    return {'db': roommate_db, 'User': User, 'Review': Review}
