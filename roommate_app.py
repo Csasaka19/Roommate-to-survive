1e87e74eabc15cc73156c7c433391ed193c43a90
Clive Sasaka <clivesasaka@gmail.com>
2023-08-26 22:04:37 +0300

    Include booking class to the shell context

--- roommate_app.py
+++ roommate_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''This module defines the application object for the application'''
 from roommate_app import roommate_app, roommate_db
-from roommate_app.models import User, Review
+from roommate_app.models import User, Review, Booking
 
 
 # This decorator registers a function to be executed right before the application receives a request
@@ -10,4 +10,4 @@ from roommate_app.models import User, Review
 # Run the following command in the terminal to start the shell: $ flask shell
 def make_shell_context():
     '''This function creates a shell context for the application'''
-    return {'db': roommate_db, 'User': User, 'Review': Review}
+    return {'db': roommate_db, 'User': User, 'Review': Review, 'Booking': Booking}
